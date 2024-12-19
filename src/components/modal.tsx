32de365e2f2299ed33b90213c6b887557089980c
Matheus Freitas <matheusmatheusfdc10@hotmail.com>
2024-12-19 11:21:55 -0300

    update

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -27,26 +27,30 @@ const Modal = ({
         document.body.classList.remove('overflow-hidden')
     }
 
-    if (isOpen) {
-        return (
-            <div className="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm overflow-y-auto grid items-center">
-                <div className="relative w-full h-auto max-w-lg mx-auto bg-zinc-100 rounded-lg shadow-lg p-8 my-8">
-                    <button
+    if (!isOpen) return null;
+
+    return (
+        <div
+            onClick={() => onClose(false)} 
+            className="fixed inset-0 z-50 bg-black/50 backdrop-blur-sm overflow-y-auto grid items-center"
+        >
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className="relative w-full h-auto max-w-lg mx-auto bg-zinc-100 rounded-lg shadow-lg p-8 my-8"
+            >
+                <button
                     onClick={() => onClose(false)}
                     className="absolute top-2 right-3 text-gray-500 hover:text-gray-800 text-xl"
-                    >
-                        ✖
-                    </button>
-    
-                    <div className="relative">
-                        {children}
-                    </div>
+                >
+                    ✖
+                </button>
+
+                <div className="relative">
+                    {children}
                 </div>
             </div>
-        )
-    } else {
-        return null;
-    }
+        </div>
+    )
 }
 
 export default Modal;
